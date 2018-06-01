16faba5d2a5a4548b6ea3803588faf9ffdbe8c6e
Lincoln Wisely <lincolnwisely@gmail.com>
2018-06-01 10:26:46 -0500

    Add package.json, clean up app.js

--- notes-node/app.js
+++ notes-node/app.js
@@ -1,17 +1,15 @@
 console.log('Starting app.js');
 
 const fs = require('fs');
-const os = require('os');
+const _ = require('lodash');
 const notes = require('./notes.js');
 
-// var user = os.userInfo();
-//
-// // console.log(user);
-//
-// fs.appendFileSync('greetings.txt', `Hello ${user.username}! You are ${notes.age}.`);
+// var filteredArray = _.uniq(['Linc', 2, 'Linc', 1, 2, 3, 2, 2, 2, 'Vince', 'Arin']);
+// console.log(filteredArray);
 
-var result = notes.addNote();
-console.log(result);
 
-var addNums = notes.add(10, 20);
-console.log('Result:', notes.add(10, -30));
+// var result = notes.addNote();
+// console.log(result);
+
+// var addNums = notes.add(10, 20);
+// console.log('Result:', notes.add(10, -30));
